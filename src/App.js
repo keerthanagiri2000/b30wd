569ed759f5b4fa57939ac7eea5b3175f4a5fb699
keerthanagiri2000 <keerthanagiri2000@gmail.com>
2022-01-30 21:35:38 +0530

    removed code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./App.css";
+export default function App() {
+  // const names = ["one", "two", "three", "four", "five"];
+  const names = [
+    {
+      name: "keerthana",
+      src:
+        "https://i.pinimg.com/564x/27/66/d1/2766d1ce71b173d6949775804a38563f.jpg"
+    },
+    {
+      name: "keerthu",
+      src:
+        "https://i.pinimg.com/originals/89/54/13/895413bcffa166f4a192fd2773be211e.jpg"
+    },
+    {
+      name: "keerthi",
+      src:
+        "https://www.whatsappprofiledpimages.com/wp-content/uploads/2021/11/Cute-Cartoon-Whatsapp-DP-Wallpaper-Pics-Download.png"
+    }
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*<Welcome
+        name="keerthana"
+        src="https://i.pinimg.com/564x/27/66/d1/2766d1ce71b173d6949775804a38563f.jpg"
+      />
+      <Welcome
+        name="keerthu"
+        src="https://i.pinimg.com/originals/89/54/13/895413bcffa166f4a192fd2773be211e.jpg"
+      />
+      <Welcome
+        name="keerthi"
+        src="https://www.whatsappprofiledpimages.com/wp-content/uploads/2021/11/Cute-Cartoon-Whatsapp-DP-Wallpaper-Pics-Download.png"
+      /> */}
+      {names.map((nm) => (
+        <Welcome name={nm.name} src={nm.src} />
+      ))}
     </div>
   );
 }
+//const name = "keerthana";
 
-export default App;
+//return (
+// <div>
+// <h1>Hello, {props.name}😊😊</h1>
+
+// </div>
+//);
+//}
+function Msg({ name }) {
+  return (
+    <div>
+      <h1>Hello, {name}😉😉🎶</h1>
+    </div>
+  );
+}
+function Welcome({ name, src }) {
+  return (
+    <div className="welcome">
+      <img src={src} alt="image" />
+      <h1>Hello,{name}😊😊</h1>
+    </div>
+  );
+}
